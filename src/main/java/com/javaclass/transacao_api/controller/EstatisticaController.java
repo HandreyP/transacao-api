9ccf4f317f9f2fe1127877364263f45be8f9affc
HandreyP <hrpereira@ua.pt>
2025-03-15 17:54:50 +0000

    JavaClass - created swagger

--- src/main/java/com/javaclass/transacao_api/controller/EstatisticaController.java
+++ src/main/java/com/javaclass/transacao_api/controller/EstatisticaController.java
@@ -2,8 +2,12 @@ package com.javaclass.transacao_api.controller;
 
 import com.javaclass.transacao_api.business.services.EstatisticasService;
 import com.javaclass.transacao_api.controller.dtos.EstatisticasResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,14 @@ public class EstatisticaController {
 
     private final EstatisticasService estatisticasService;
 
+    @GetMapping
+    @Operation(description = "Endpoint responsável por buscar estatisticas transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do servidor")
+
+    })
     public ResponseEntity<EstatisticasResponseDto> buscarEstatisticas(
             @RequestParam(value = "deltaBusca", required = false, defaultValue = "60") Integer deltaBusca){
 
--- src/main/java/com/javaclass/transacao_api/controller/TransacaoController.java
+++ src/main/java/com/javaclass/transacao_api/controller/TransacaoController.java
@@ -2,6 +2,9 @@ package com.javaclass.transacao_api.controller;
 
 import com.javaclass.transacao_api.business.services.TransacaoService;
 import com.javaclass.transacao_api.controller.dtos.TransacaoRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,14 @@ public class TransacaoController {
     private final TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(description = "Endpoint responsável por adicionar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação Gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno")
+
+    })
     public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDto transacaoDto){
         transacaoService.adicionarTransacao(transacaoDto);
 
@@ -22,6 +33,13 @@ public class TransacaoController {
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsável por deletar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação deletar com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno")
+
+    })
     public ResponseEntity<Void> deletarTransacoes(){
         transacaoService.limparTransacao();
 
